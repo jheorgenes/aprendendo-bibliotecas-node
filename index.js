dfefc45c5b1d3abb12173831fa66f8251c9d4d89
Jheorgenes Warlley Dias da Silva <jheorgenes@gmail.com>
2023-04-08 12:10:39 -0300

    [08-04-2023] Primeira biblioteca para leitura de arquivo e interpretacao de regex

---
+++ index.js
@@ -0,0 +1,53 @@
+import chalk from 'chalk';
+import fs from 'fs';
+
+function extraiLinks(texto){
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const capturas = [...texto.matchAll(regex)];
+  const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+  return resultados;
+}
+
+function trataErro(erro){
+  throw new Error(chalk.red(erro.code, 'não há arquivo no diretório!'));
+}
+
+// função síncrona
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto)=> {
+//     if(erro){
+//       trataErro(erro);
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
+
+//função assíncrona usando promisses e then
+// function pegaArquivo(caminhoDoArquivo){
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(caminhoDoArquivo, encoding)
+//     .then((texto)=> console.log(chalk.green(texto)))
+//     .catch(trataErro);
+// }
+
+//função assíncrona usando async/await
+async function pegaArquivo(caminhoDoArquivo){
+  try {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+    console.log(extraiLinks(texto));
+  } catch(erro){
+    trataErro(erro);
+  }
+}
+
+pegaArquivo('./arquivos/texto.md');
+// pegaArquivo('./arquivos/');
+
+// Trabalhando com regex
+// \[[^[\]]*?\]
+// \(https?:\/\/[^\s?#.].[^\s]*\)
+
+
